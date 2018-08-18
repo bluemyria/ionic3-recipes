7eed1ebf83389ac48c26053d5f6043203db55b57
bluemyria <mlekkou@gmail.com>
2018-08-18 13:40:29 +0200

    added firebase and components/modules in app.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,40 +1,62 @@
 import { NgModule, ErrorHandler } from '@angular/core';
+// import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http'
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
+import { ShoppingListPage } from '../pages/shopping-list/shopping-list';
+import { RecipesPage } from '../pages/recipes/recipes';
+import { RecipePage } from '../pages/recipe/recipe';
+import { EditRecipePage } from '../pages/edit-recipe/edit-recipe';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+import { ShoppingListService } from '../services/shopping-list.service';
+import { RecipeService } from '../services/recipe.service';
+import { SigninPage } from '../pages/signin/signin';
+import { SignupPage } from '../pages/signup/signup';
+import { AuthService } from '../services/auth.service';
+import { SLOptionsPage } from '../pages/shopping-list/sl-options/sl-options';
+
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage
+    TabsPage,
+    ShoppingListPage,
+    RecipesPage,
+    RecipePage,
+    EditRecipePage,
+    SigninPage,
+    SignupPage,
+    SLOptionsPage
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage
+    TabsPage,
+    ShoppingListPage,
+    RecipesPage,
+    RecipePage,
+    EditRecipePage,
+    SigninPage,
+    SignupPage,
+    SLOptionsPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    { provide: ErrorHandler, useClass: IonicErrorHandler },
+    ShoppingListService,
+    RecipeService,
+    AuthService
   ]
 })
-export class AppModule {}
+export class AppModule { }
